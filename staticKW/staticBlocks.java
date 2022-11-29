d8bb0a04a2294914b0b37485805359e99850e8af
Avdhut Malankar <avdhut.satish@gmail.com>
2022-11-29 13:05:31 +0530

    static blocks updated

--- staticKW/staticBlocks.java
+++ staticKW/staticBlocks.java
@@ -1,36 +1,40 @@
 package staticKW;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+ * It wouldn't be possible to initialize a List object with all the initial values along with 
+ * declaration. So, this is why we've utilized the static block here.
+ */
 public class staticBlocks {
-  static int a = 23;
-  static int b;
-  static int max;
 
-  // static blocks
+  public static List<String> ranks = new ArrayList<>();
+
   static {
-    System.out.println("First Static block.");
-    b = a * 4;
-  }
-  static {
-    System.out.println("Second Static block.");
-    max = 30;
+    ranks.add("Lieutenant");
+    ranks.add("Captain");
+    ranks.add("Major");
   }
 
-  // static method
+  {
+    ranks.add("Colonel");
+    ranks.add("General");
+  }
+
   static void display() {
-    System.out.println("a = " + a);
-    System.out.println("b = " + b);
-    System.out.println("max = " + max);
+    System.out.println(ranks);
   }
 
   public static void main(String args[]) {
-    /* 
-     * static block is executed when you call a static method or use a static variable or create an object of the class.
+    /*
+     * static block is executed when you call a static method or use a static
+     * variable or create an object of the class.
      * staticBlocks obj=new staticBlocks();
      * staticBlocks.display();
-     * System.out.println(staticBlocks.a);
+     * System.out.println(staticBlocks.ranks);
      * will give same output.
-    */
+     */
     staticBlocks.display();
-
   }
 }
