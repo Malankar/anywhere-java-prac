0269332c7db2f0f69fb6ca69bcbba16faa1f5985
Avdhut Malankar <avdhut.satish@gmail.com>
2022-12-01 19:58:36 +0530

    Access modifiers done

--- accessModifiers/protectedModifier/FBISecretAgent.java
+++ accessModifiers/protectedModifier/FBISecretAgent.java
@@ -2,7 +2,7 @@ package accessModifiers.protectedModifier;
 
 public class FBISecretAgent extends SecretAgent {
     public static void main(String[] args) {
-        SecretAgent fbiagent = new SecretAgent();
+        
         System.out.println(SecretAgent.agentCount);
     }
 }
---
+++ accessModifiers/protectedModifier/Main.java
@@ -0,0 +1,10 @@
+package accessModifiers.protectedModifier;
+
+public class Main {
+  public static void main(String[] args) {
+    MI6SecretAgent mi6agent = new MI6SecretAgent();
+    SecretAgent fbiagent = new SecretAgent();
+    System.out.println(""+mi6agent+fbiagent+"\n");
+    System.out.println(SecretAgent.agentCount);
+  }
+}
