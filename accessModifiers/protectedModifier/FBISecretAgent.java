a872dac2fc92f80a474f9e0994697c44f2c819da
Avdhut <32304774+Malankar@users.noreply.github.com>
2022-12-05 16:05:50 +0530

    Delete accessModifiers directory

--- accessModifiers/protectedModifier/FBISecretAgent.java
+++
@@ -1,8 +0,0 @@
-package accessmodifiers.protectedmodifier;
-
-public class FBISecretAgent extends SecretAgent {
-    public static void main(String[] args) {
-
-        System.out.println(SecretAgent.agentCount);
-    }
-}
--- accessModifiers/protectedModifier/SecretAgent.java
+++
@@ -1,9 +0,0 @@
-package accessmodifiers.protectedmodifier;
-
-public class SecretAgent {
-  protected static int agentCount = 0;
-
-  SecretAgent() {
-    SecretAgent.agentCount++;
-  }
-}
